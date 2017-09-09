32ac550a2731f8b43a44ddcfcea76c1e031e9a99
rlondner <raphael@londner.net>
2017-09-08 18:16:54 -0700

    Add project files.

---
+++ MongoDB.Samples.AggregationFramework.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Samples.AggregationFramework.Library;
+using System;
+using System.IO;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB.Samples.AggregationFramework.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var mdbSettings = new MongoDBSettings();
+            configuration.GetSection("MongoDB").Bind(mdbSettings);
+
+            Console.WriteLine($"Cluster Connection Uri is '{mdbSettings.ConnectionUri}'");
+            Console.WriteLine($"DB Database Name is '{mdbSettings.DatabaseName}'");
+            Console.WriteLine($"DB Collection Name is '{mdbSettings.CollectionName}'");
+
+            DbManager dbMgr = new DbManager(mdbSettings.ConnectionUri, mdbSettings.DatabaseName);
+            var collection = dbMgr.GetCollection(mdbSettings.CollectionName);
+
+            //int colCount = collection.Count();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MongoDB.Samples.AggregationFramework.Library/DbManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Collections;
+
+namespace MongoDB.Samples.AggregationFramework.Library
+{
+    public class DbManager
+    {
+
+        private volatile IMongoCollection<BsonDocument> m_collection;
+        private static object syncRoot = new Object();
+
+        private string m_ConnectionUri;
+        private string m_DatabaseName;
+
+        private DbManager() { }
+
+        public DbManager(string strConnectionUri, string strDatabaseName)
+        {
+            m_ConnectionUri = strConnectionUri;
+            m_DatabaseName = strDatabaseName;
+        }
+
+        public IMongoCollection<BsonDocument> GetCollection(string collectionName)
+        {
+            if (m_collection == null)
+            {
+                lock (syncRoot)
+                {
+                    if (m_collection == null)
+                    {
+                        var client = new MongoClient(m_ConnectionUri);
+                        var db = client.GetDatabase(m_DatabaseName);
+                        m_collection = db.GetCollection<BsonDocument>(collectionName);
+                    }
+                }
+            }
+            return m_collection;
+
+        }
+    }
+}
